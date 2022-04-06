eb4435924ba63de0da9e07d87bc4fdceb2ce0a04
Diego Sosa <dhsosa98@gmail.com>
2022-04-06 02:20:40 -0300

    created 'findOne() and findAll()' Products services

---
+++ src/services/products.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { Product } from './../entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'bla bla',
+      price: 122,
+      image: '',
+      stock: 12,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'bla bla 2',
+      price: 433,
+      image: '',
+      stock: 9,
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product #${id} not found`);
+    }
+    return product;
+  }
+}
