574351e6b81c467a284b78615a73c048d1edc827
Diego Sosa <dhsosa98@gmail.com>
2022-06-01 01:44:28 -0300

    change exception

--- src/common/parse-int.pipe.ts
+++ src/common/parse-int.pipe.ts
@@ -5,7 +5,7 @@ export class ParseIntPipe implements PipeTransform {
   transform(value: string) {
     const val = parseInt(value, 10);
     if (isNaN(val)) {
-      throw new BadRequestException(`${value} is not an number`);
+      throw new BadRequestException(`${value} is not a number`);
     }
     return val;
   }
